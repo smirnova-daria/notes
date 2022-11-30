90f99665aa5f52ca0ab3a05cc47cf5eb5bcee609
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-30 22:13:19 +0300

    add feature to create a note

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,154 @@
 <template>
-  <main>Home</main>
+  <main>
+    <div class="flex items-center justify-between mb-8">
+      <h1 class="text-3x1 font-bold">My Notes App</h1>
+      <button
+        @click="logout"
+        class="text-red-500 hover:underline cursor-pointer"
+      >
+        Logout
+      </button>
+    </div>
+
+    <form @submit.prevent="handleCreateNote" class="mb-8">
+      <label class="block mb-4">
+        <span class="block text-sm uppercase mb-2">Title</span>
+        <input
+          type="text"
+          v-model="newNote.title"
+          placeholder="What's the title"
+          class="block w-full text-slate-800 px-4 py-2"
+        />
+      </label>
+      <label class="block mb-4">
+        <span class="block text-sm uppercase mb-2">Content</span>
+        <textarea
+          v-model="newNote.content"
+          placeholder="What's the content"
+          class="block w-full text-slate-800 px-4 py-2"
+        ></textarea>
+      </label>
+
+      <input
+        type="submit"
+        value="Create note"
+        class="text-green-500 hover:underline mb-4 cursor-pointer"
+      />
+    </form>
+
+    <div v-if="!notesLoading">
+      <div
+        v-for="note in notesResult.notes"
+        :key="note.id"
+        class="relative bg-white text-slate-700 rounded-lg p-6 mb-6"
+      >
+        <button
+          class="text-red-500 absolute top-6 right-6 font-bold"
+          @click="() => deleteNote({ id: note.id })"
+        >
+          Delete
+        </button>
+
+        <h3 class="font-bold text-2x1 mb-4">{{ note.title }}</h3>
+        <p
+          class="text-lg text-gray-500 mb-4"
+          v-html="convertToHTML(note.content)"
+        ></p>
+        <div class="text-sm text-gray-500 italic">
+          {{ convertToDate(note.created) }}
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useSignOut, useUserId } from "@nhost/vue";
+import { useMutation, useQuery } from "@vue/apollo-composable";
+import { gql } from "@apollo/client/core";
+
+const router = useRouter();
+const { signOut } = useSignOut();
+const userId = useUserId();
+
+const newNote = ref({
+  title: "",
+  content: "",
+});
+
+const logout = () => {
+  signOut();
+
+  router.push("/login");
+};
+
+const {
+  loading: notesLoading,
+  result: notesResult,
+  refetch: notesRefetch,
+} = useQuery(
+  gql`
+    query ($userId: String!) {
+      notes(order_by: { created: desc }, where: { user_id: { _eq: $userId } }) {
+        id
+        title
+        content
+        created
+      }
+    }
+  `,
+  {
+    userId: userId.value,
+  }
+);
+
+const { mutate: createNote, onDone: createNoteDone } = useMutation(gql`
+  mutation ($userId: String!, $title: String!, $content: String!) {
+    insert_notes_one(
+      object: { content: $content, user_id: $userId, title: $title }
+    ) {
+      id
+    }
+  }
+`);
+
+const handleCreateNote = () => {
+  if (!newNote.value.title || !newNote.value.content) {
+    return alert("Please fill in all fields");
+  }
+
+  createNote({
+    userId: userId.value,
+    title: newNote.value.title,
+    content: newNote.value.content,
+  });
+};
+
+createNoteDone(() => {
+  notesRefetch();
+  newNote.value = {
+    title: "",
+    content: "",
+  };
+});
+
+const { mutate: deleteNote, onDone: deleteDone } = useMutation(gql`
+  mutation ($id: Int!) {
+    delete_notes(where: { id: { _eq: $id } }) {
+      affected_rows
+    }
+  }
+`);
+
+deleteDone(() => {
+  notesRefetch();
+});
+
+const convertToHTML = (content) => content.replace(/\n/g, "<br />");
+
+const convertToDate = (date) => new Date(date).toLocaleString();
 </script>
 
 <style></style>
