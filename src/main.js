f33233b365e305eb5a7eb7dcb27dbb97bb59e857
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-30 20:30:44 +0300

    add nhost, router, apolloClient

--- src/main.js
+++ src/main.js
@@ -1,5 +1,55 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./main.css";
 
-createApp(App).mount('#app')
+import { createApolloClient } from "@nhost/apollo";
+import { NhostClient } from "@nhost/vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+const nhost = new NhostClient({
+  subdomain: "hkpisoegxjieyxffjpvk",
+  region: "eu-central-1",
+});
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("./views/Home.vue"),
+    meta: {
+      protected: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("./views/Login.vue"),
+  },
+];
+
+const apolloClient = createApolloClient({ nhost });
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.protected)) {
+    const isAuthenticated = await nhost.auth.isAuthenticatedAsync();
+
+    if (!isAuthenticated) {
+      return next("/login");
+    }
+  }
+
+  next();
+});
+
+createApp(App)
+  .provide(DefaultApolloClient, apolloClient)
+  .use(nhost)
+  .use(router)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view></router-view>
+  <main>Home</main>
 </template>
 
 <script>
